4c3c8fd53795945b5954e588b3ce278be904e62e
Satyamkumarjha4 <satyamjha4@gmail.com>
2024-08-30 13:50:59 +0530

    comparision and logical operators covered

--- operation.js
+++ operation.js
@@ -18,3 +18,45 @@ console.log("updated a = ", a);                                                 
 console.log("--a = ", --a);                                                     //pre decrement
 console.log("updated a = ", a);                                                 //new value of a after operation
 
+//assignment operation
+let x = 10;
+
+console.log("x = ", x);
+x += 5;                                                                         //x = x + 5
+console.log("x += 5 = ", x);
+x -= 5;                                                                         //x = x - 5
+console.log("x -= 5 = ", x);
+x *= 5;                                                                         //x = x * 5
+console.log("x *= 5 = ", x);
+x /= 5;                                                                         //x = x / 5
+console.log("x /= 5 = ", x);
+x %= 4;                                                                         //x = x % 4
+console.log("x %= 4 = ", x);
+x **= 5;                                                                        //x = x ** 5
+console.log("x **= 5 = ", x);
+
+//comparison operation
+let p = 10;
+let q = 20;
+let o = "10"; 
+
+console.log("p = ", p, " q = ", q);
+console.log("p == q = ", p == q);                                               //equal to
+console.log("p != q = ", p != q);                                               //not equal to
+console.log("p > q = ", p > q);                                                 //greater than
+console.log("p < q = ", p < q);                                                 //less than
+console.log("p >= q = ", p >= q);                                               //greater than or equal to
+console.log("p <= q = ", p <= q);                                               //less than or equal to
+console.log("p == o = ", p == o);                                               //equal to
+console.log("p === o = ", p === o);                                             //strict equal to
+console.log("p != o = ", p != o);                                               //not equal to
+console.log("p !== o = ", p !== o);                                             //strict not equal to
+
+//logical operation
+let r = true;
+let s = false;
+
+console.log("r = ", r, " s = ", s);
+console.log("r && s = ", r && s);                                               //logical AND
+console.log("r || s = ", r || s);                                               //logical OR
+console.log("!r = ", !r);                                                       //logical NOT
