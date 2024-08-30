d0f691f537f4868420eb0dd28aaf179e7497499d
Satyamkumarjha4 <satyamjha4@gmail.com>
2024-08-30 12:24:51 +0530

    entering data in diferent data types

--- Hello.js
+++ Hello.js
@@ -1,2 +1,14 @@
+//First Program: Hello World
 console.log('Hello World');
+
+//Variables and Data Types
+fullname = 'John Doe';                                // String
+age = 30;                                             // Number
+console.log("Name: ",fullname,"  Age: ", age);
+
+raduis = 13.5;                                        // Float
+a = null;                                             // Null
+b = undefined;                                        // Undefined
+isname = true;                                        //boolean
+console.log("Raduis: ",raduis,"  Null: ", a,"  Undefined: ", b,"  Boolean: ", isname);
 
