a81125ef6d814bbac31b89366e1a4d45d2da28ee
Satyamkumarjha4 <satyamjha4@gmail.com>
2024-08-30 12:35:28 +0530

    use of let and const

--- Hello.js
+++ Hello.js
@@ -12,3 +12,23 @@ b = undefined;                                        // Undefined
 isname = true;                                        //boolean
 console.log("Raduis: ",raduis,"  Null: ", a,"  Undefined: ", b,"  Boolean: ", isname);
 
+//Use of let and const
+let x = 5;
+console.log(x);
+x = 10;                                               //Reassign is allowed
+console.log(x);
+
+const PI = 3.14;                                      //Reassign is not allowed
+console.log(PI);
+
+let y;                                                //Declaration without initialization is allowed for let
+console.log(y);                                       //Undefined is printed as the data type is not assigned
+
+let z = 12;                                           //Global scope 
+console.log(z);
+{
+    let z = 13;                                       //Block level scope
+    console.log(z);
+}
+console.log(z);                                       //let and const have block level scope
+
