03200d90823bfeae6399098d2f8d7badd7be6d86
Satyamkumarjha4 <satyamjha4@gmail.com>
2024-08-30 13:33:54 +0530

    the use of unary arithmetic operator

--- operation.js
+++ operation.js
@@ -9,4 +9,12 @@ console.log("a / b = ", a / b);                                                 
 console.log("a % b = ", a % b);                                                 //modulus or remainder
 console.log("a ^ b = ", a ** b);                                                //exponentiation
 
+console.log("a++ = ", a++);                                                     //post increment
+console.log("updated a = ", a);                                                 //new value of a after operation
+console.log("a-- = ", a--);                                                     //post decrement
+console.log("updated a = ", a);                                                 //new value of a after operation
+console.log("++a = ", ++a);                                                     //pre increment
+console.log("updated a = ", a);                                                 //new value of a after operation
+console.log("--a = ", --a);                                                     //pre decrement
+console.log("updated a = ", a);                                                 //new value of a after operation
 
