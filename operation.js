617754d59506575bd1a1e799f963e714f20992e5
Satyamkumarjha4 <satyamjha4@gmail.com>
2024-08-30 13:23:06 +0530

    the use of arithmetic operator

---
+++ operation.js
@@ -0,0 +1,12 @@
+//arithmetic operation
+let a = 10;
+let b = 20;
+
+console.log("a + b = ", a + b);                                                 //addition
+console.log("a - b = ", a - b);                                                 //subtraction
+console.log("a * b = ", a * b);                                                 //multiplication
+console.log("a / b = ", a / b);                                                 //division
+console.log("a % b = ", a % b);                                                 //modulus or remainder
+console.log("a ^ b = ", a ** b);                                                //exponentiation
+
+
